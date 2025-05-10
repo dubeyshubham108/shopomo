7e1b2b04038bf8883510e1590c7f51979770e6d9
dubeyshubham108 <dubeyshubham154@gmail.com>
2025-05-10 11:38:22 +0530

    Added the assets and created the Navbar

--- shopomo-frontend/src/App.jsx
+++ shopomo-frontend/src/App.jsx
@@ -1,35 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Login from "./Pages/Login";
+import Home from "./Pages/Home";
+import Cart from "./Pages/Cart";
+import About from "./Pages/About";
+import Collection from "./Pages/Collection";
+import Contact from "./Pages/Contact";
+import PlaceOrder from './Pages/PlaceOrder';
+import Order from './Pages/Order';
+import Product from './Pages/Product';
+import Navbar from "./components/Navbar";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className='px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]'>
+      <Navbar/>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/login" element={<Login/>}/>
+        <Route path="/cart" element={<Cart/>} />
+        <Route path="/About" element={<About/>} />
+        <Route path="/collection" element={<Collection/>} />
+        <Route path="/contact" element={<Contact/>} />
+        <Route path="/place-order" element={<PlaceOrder/>} />
+        <Route path="/product" element={<Product/>} />
+        <Route path="/order" element={<Order/>} />
+      </Routes>
+    </div>
   )
-}
+};
 
 export default App
