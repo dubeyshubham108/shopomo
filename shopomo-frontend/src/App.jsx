9447cff19fc6eb26da8dd7a618c5867b37d9b4bb
dubeyshubham108 <dubeyshubham154@gmail.com>
2025-05-25 19:17:02 +0530

    setup an order page

--- shopomo-frontend/src/App.jsx
+++ shopomo-frontend/src/App.jsx
@@ -16,7 +16,6 @@ import { ToastContainer, toast } from 'react-toastify';
 // import 'react-toastify/dist/ReactToastify.css';
 
 
-
 const App = () => {
   return (
     <div className='px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]'>
--- shopomo-frontend/src/context/ShopContext.jsx
+++ shopomo-frontend/src/context/ShopContext.jsx
@@ -1,6 +1,7 @@
 import React, { createContext, useEffect, useState } from "react";
 import { products } from '../assets/frontend_assets/assets';
 import { toast } from "react-toastify";
+import { useNavigate } from "react-router-dom";
 
 export const ShopContext = createContext();
 
@@ -11,6 +12,7 @@ const ShopContextProvider = ( props ) => {
     const [ search, setSearch ] = useState('');
     const [ showSearch, setShowSearch ] = useState(false);
     const [ cartItems, setCartItems ] = useState({});
+    const navigate = useNavigate();
 
     const addToCart = async (itemId, size) => {
 
@@ -58,7 +60,7 @@ const ShopContextProvider = ( props ) => {
         setCartItems(cartData);
     }
 
-    const getCartTotal = async () => {
+    const getCartAmount = () => {
         let totalAmount = 0;
         for(const items in cartItems) {
             let itemInfo = products.find((product) => product._id === items);
@@ -79,7 +81,7 @@ const ShopContextProvider = ( props ) => {
         products, currency, delivery_fee,
         search, setSearch, showSearch, setShowSearch,
         cartItems, addToCart, getCartCount, updateQuantity,
-        getCartTotal
+        getCartAmount, navigate
     }
 
     return (
