2a45144324fc8d653d51d5f28392d2ac9a22cd12
dubeyshubham108 <dubeyshubham154@gmail.com>
2025-05-26 09:18:50 +0530

    Created Contact Us, Profile Page and About Page

--- shopomo-frontend/src/components/Navbar.jsx
+++ shopomo-frontend/src/components/Navbar.jsx
@@ -46,7 +46,7 @@ const Navbar = () => {
             <div className="flex item-center gap-6"> 
                 <img onClick={() => setShowSearch(true)} src={search_icon} className="w-4 cursor-pointer" alt="Cart icon" />
                 <div className="group relative">
-                    <img className="w-5.5 cursor-pointer" src={profile} alt="Profile icon" />
+                    <Link to='/login'><img className="w-5.5 cursor-pointer" src={profile} alt="Profile icon" /></Link>
                     <div className='group-hover:block hidden absolute dropdown-menu right-0 pt-4'>
                         <div className="flex flex-col gap-2 w-36 py-3 px-5 bg-slate-100 text-gray-500 rounded">
                             <p className='cursor-pointer hover:text-black'>My Profile</p>
