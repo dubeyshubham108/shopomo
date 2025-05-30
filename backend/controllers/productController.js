f220a88085cde401165e6319f1a169c2a619d6b1
dubeyshubham108 <dubeyshubham154@gmail.com>
2025-05-30 09:17:54 +0530

    added cloudinary function for uploading the image in cloudinary

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,3 +1,4 @@
+import { v2 as cloudinary } from "cloudinary"
 
 // function for add product
 const addProduct = async ( req, res) => {
@@ -10,8 +11,17 @@ const addProduct = async ( req, res) => {
         const image3 = req.files.image3 && req.files.image3[0];
         const image4 = req.files.image4 && req.files.image4[0];
 
+        const images = [image1, image2, image3, image4].filter((item) => item !== undefined);
+
+        let imagesUrl = await Promise.all(
+            images.map(async (item) => {
+                let result = await cloudinary.uploader.upload(item.path, {resource_type: 'image'});
+                return result.secure_url
+            })
+        )
+
         console.log(name, description, price, category, subCategory, sizes, bestseller);
-        console.log(image1, image2, image3, image4)
+        console.log(imagesUrl)
 
         res.json({})
 
