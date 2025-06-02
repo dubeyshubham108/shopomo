fbddd191436da6552e6f0ddc37d1cbfc90bf0b32
dubeyshubham108 <dubeyshubham154@gmail.com>
2025-06-02 16:10:53 +0530

    Added the product details in db

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,9 +1,9 @@
 import { v2 as cloudinary } from "cloudinary"
+import productModel from "../models/productModel.js";
 
 // function for add product
 const addProduct = async ( req, res) => {
     try {
-
         const { name, description, price, category, subCategory, sizes, bestseller } = req.body;
 
         const image1 = req.files.image1 && req.files.image1[0];
@@ -13,20 +13,34 @@ const addProduct = async ( req, res) => {
 
         const images = [image1, image2, image3, image4].filter((item) => item !== undefined);
 
-        let imagesUrl = await Promise.all(
+        let imageUrl = await Promise.all(
             images.map(async (item) => {
                 let result = await cloudinary.uploader.upload(item.path, {resource_type: 'image'});
                 return result.secure_url
             })
         )
 
-        console.log(name, description, price, category, subCategory, sizes, bestseller);
-        console.log(imagesUrl)
+        const productData = {
+            name,
+            description,
+            category,
+            price: Number(price),
+            subCategory,
+            bestseller: bestseller === 'true' ? true : false,
+            sizes: JSON.parse(sizes),
+            image: imageUrl,
+            date: Date.now()
+        }
 
-        res.json({})
+        console.log(productData);
+
+        const product = new productModel(productData);
+        await product.save();
+
+        res.json({ success: true, message: "Product Added" })
 
     } catch (error) {
-        console.log(err);
+        console.log(error);
         res.json({success: false, message: error.message})
     }
 }
