4cff96b49f095a40fe961cacee40ced69f82bfd0
dubeyshubham108 <dubeyshubham154@gmail.com>
2025-06-02 16:39:41 +0530

    created a removed product api for removing product

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -47,12 +47,25 @@ const addProduct = async ( req, res) => {
 
 // function for list product
 const listProducts = async (req, res) => {
-
+    try {
+        const products = await productModel.find({});
+        res.json({success: true, products})
+    } catch(error) {
+        console.log(error)
+        res.json({success: false, message: error.message})
+    }
 }
 
 // function for remove product
 const removeProducts = async (req, res) => {
+    try {
+        await productModel.findByIdAndDelete(req.body.id)
+        res.json({success: true, message: "Product Removed"})
 
+    } catch(error) {
+        console.log(error.message)
+        res.json({success: fasle, message: error.message })
+    }
 }
 
 // function for single product info
